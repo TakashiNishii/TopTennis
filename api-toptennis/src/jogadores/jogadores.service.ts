aaca1bf83879ca79325fb8cd8b1557900e494133
TakashiNishii <ygortn14@gmail.com>
2025-04-20 21:29:48 -0400

    :tada: Init project and add Jogadores Service

---
+++ api-toptennis/src/jogadores/jogadores.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Jogador } from './interfaces/jogador.interface';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
+    this.criar(criaJogadorDto);
+  }
+
+  private criar(criaJogadorDto: CriarJogadorDto): void {
+    const { nome, telefoneCelular, email } = criaJogadorDto;
+    const jogador: Jogador = {
+      _id: uuidv4(),
+      nome,
+      telefoneCelular,
+      email,
+      ranking: 'A',
+      posicaoRanking: 1,
+      urlFotoJogador: 'https://www.google.com/foto123.jpg',
+    };
+    this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
+    this.jogadores.push(jogador);
+  }
+}
